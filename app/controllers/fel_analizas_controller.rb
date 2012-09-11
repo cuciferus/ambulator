042b6c1c5a018d383cf75c4fd552c885d0e82087
Cucu Ionut <ionutcristiancucu@gmail.com>
2012-09-11 23:06:27 +0300

    am facut search de felanaliza dar intoarce pacienti??

--- app/controllers/fel_analizas_controller.rb
+++ app/controllers/fel_analizas_controller.rb
@@ -1,2 +1,5 @@
 class FelAnalizasController < InheritedResources::Base
+  def index
+    @fel_analizas = FelAnaliza.search(params[:search])
+  end
 end
--- app/models/fel_analiza.rb
+++ app/models/fel_analiza.rb
@@ -3,4 +3,16 @@ class FelAnaliza < ActiveRecord::Base
   attr_accessible :nume, :unitate_masura, :standard, :valoare_minima, :valoare_maxima
   validates_presence_of :nume
   validates_uniqueness_of :nume
+  def self.search(search)
+    search = nil if search ==""
+    if search
+      @fel_analizas = find(:all, :conditions => ['nume like ?',"%#{search}"])
+    else
+      @fel_analizas = find(:all, :limit => 10)
+    end
+  end
+
+    
+
+
 end
