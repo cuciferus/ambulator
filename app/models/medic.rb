be0f5efb5e0943b7dbf6a915cbd2d6ed9a7c1b66
Cucu Ionut <ionutcristiancucu@gmail.com>
2012-11-12 22:59:11 +0200

    white list

--- app/models/medic.rb
+++ app/models/medic.rb
@@ -1,6 +1,7 @@
 class Medic < ActiveRecord::Base
   has_one :hospital
-  accepts_nested_attributes_for :hospital
+  has_many :retetars
+  accepts_nested_attributes_for :hospital, :retetars
   attr_accessible :cod_parafa, :nume, :prenume
   validates :nume, :presence => true
   validates :prenume, :presence => true
