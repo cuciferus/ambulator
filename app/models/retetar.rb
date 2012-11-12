e8e48f262666f9226018d5608025044a4bec8013
Cucu Ionut <ionutcristiancucu@gmail.com>
2012-11-12 22:43:51 +0200

    white list si metoda pentru display in reteta

--- app/models/retetar.rb
+++ app/models/retetar.rb
@@ -1,4 +1,8 @@
 class Retetar < ActiveRecord::Base
   belongs_to :medic
   attr_accessible :tip, :medic_id, :serie, :nr_prima_reteta, :nr_ultima_reteta
+  def tip_cu_medic
+    #pentru collection_select din reteta noua
+    "#{tip} -> #{medic.try(:nume)}"
+  end
 end
