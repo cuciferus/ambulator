f8d766b6e3b8599d18fc5621e346db1dd86fda99
Cucu Ionut <ionutcristiancucu@gmail.com>
2012-11-12 22:40:29 +0200

    white list more attributes

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,5 +1,7 @@
 class Drug < ActiveRecord::Base
-  attr_accessible :cantitate, :cod, :dci, :diagnostic, :nume
+  attr_accessible :cantitate, :cod, :dci, :diagnostic, :nume, :doza
   has_many :line_drugs
   has_many :reteta, :through => :line_drugs
+
+
 end
